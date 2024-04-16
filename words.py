15b338b7ef14bb50bc70e2b0314fd919dfbc4cff
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-04-16 08:50:16 +0000

    Add three lists of words to words.py file

---
+++ words.py
@@ -0,0 +1,60 @@
+movements = [
+    "Burpee",
+    "Wallwalk",
+    "Pushup",
+    "Pullup",
+    "Clean",
+    "Snatch",   
+    "Handstand",
+    "Boxjump",
+    "Squat",
+    "Running",
+    "Rowing",
+    "Wallballs",
+    "Dips",
+    "Ropeclimb",
+    "Press",
+    "Deadlift",
+    "Situps"
+]
+
+equipment = [
+    "Kettlebell",
+    "Dumbbell",
+    "Barbell",
+    "Jumprope",
+    "Rope",
+    "Bench",
+    "Grips",
+    "Chalk",
+    "Weights",
+    "Abmat",
+    "Rower",
+    "Skierg",
+    "Bike",
+    "Box",
+    "Medball",
+    "Rings",
+    "Bar"
+]
+
+abbrevations = [
+    "HSPU",
+    "SDHP",
+    "GHD",
+    "AMRAP",
+    "EMOM",
+    "WOD",
+    "BBJO",
+    "HSW",
+    "CTB",  
+    "TTB",
+    "KBS",
+    "ATG",
+    "RMU",
+    "BMU",
+    "OHS",  
+    "GPP",
+    "TGU",
+    "OTM"
+]
