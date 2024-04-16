1762fb72fcf09ea0edc334682f303cd69071e0a3
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-04-16 09:38:00 +0000

    import libraries and files at the top of run.py and create the main function to add the name of the game and the rules at the start page

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+#import colorama
+import cat_bird
+from words import movements, equipment, abbrevations
+import word_art
+
+def game_start():
+    """
+    Shows the start page word art and the rules of the game. 
+    Prompts the user to choose a topic.
+    """
+    print(word_art.start_page)
+    print("You can choose from 3 different topics in this game:")
+    print("Movements, Equipment and Abbrevations")
+    print("They are all related to the lovely sport of CrossFit")
+    print("You have to know your CrossFit in order to save the poor bird!")
+    print("You have 6 attempts before the bird gets eaten...")
+    print("Only letters allowed and only one letter per guess")
+    print("Good luck!")
+
+game_start()
+
+
+
