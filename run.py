9ec4eb341c84ec44f5fa17afce38384257874a04
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-04-16 14:15:04 +0000

    Rearrange functions only to arrange them back to get game flow working..

--- run.py
+++ run.py
@@ -6,6 +6,8 @@ import word_art
 import os
 
 
+
+
 def validate_choice(data):
     """
     Validates the choice of topic input.
@@ -24,9 +26,13 @@ def validate_choice(data):
     return True
 
 def get_letter():
+    """
+    Prompts the user for a letter and checks that it
+    is only one letter and no numbers
+    """
     while True:
         try:
-            letter = input('Please enter a letter: \n').upper()
+            letter = input("\n\nPlease enter a letter: ").upper()
             if len(letter) == 1 and letter.isalpha():
                 break
             else:
@@ -34,12 +40,44 @@ def get_letter():
         except ValueError:
             print("Please enter a letter\n")
             
-        
+        return letter 
 
-    return letter 
+"""
+def check_letter(input_letter):
+    
+    Checks if the input letter is in the word 
+    and adds it to either the correct or wrong count
+    
+    
+    correct_count = 0
+    wrong_count = 6
+    correct = " "
+    wrong = " "
+
+    for char in word_game:
+    if(char in guessedLetters):
+      print(word_game[counter], end=" ")
+      rightLetters+=1
+    else:
+      print(" ", end=" ")
+    counter+=1
+  return rightLetters
+
+    while correct_count != len(word_game.replace(' ', '')) and wrong_count != 0:
+        display = ''
+    for letter in word_game:
+        if letter in correct:
+                display += f'{letter}'
+        else:
+                display += '_ '
+    print(display)
+"""
 
 
 def main():
+    """
+    Start page with name of the game and the game rules
+    """
     print(word_art.start_page)
     print("You can choose from 3 different topics in this game:")
     print("Movements, Equipment and Abbrevations")
@@ -50,6 +88,7 @@ def main():
     print("Good luck!\n")
 
     """
+    Asks the player to input a topic choice.
     Checks the validate_choice function and if True the while loop breaks.
     If False player get to input a choice again.
     """
@@ -61,7 +100,7 @@ def main():
         topic_input = input("Choose topic: ")
         if validate_choice(topic_input):
             break
-
+    
     """
     Picks a list with random words chosen by the
     player depending on the topic. Then a random word is generated.
@@ -71,10 +110,15 @@ def main():
         "2": equipment,
         "3": abbrevations
     }
-    
+        
     word_game = random.choice(choice_words[topic_input]).upper()
+    for x in word_game:
+        print("_", end=" ")
     
-    get_letter()
+    letter = get_letter()
+
+       
+
 
 main()
 
